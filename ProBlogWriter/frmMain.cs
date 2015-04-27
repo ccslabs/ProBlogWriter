59b8002a0148cb1b0a3c10b429ad6cdf62a5334f
ccslabs <ccslabs@hotmail.com>
2015-04-27 01:00:36 +0100

    Re-Initialised

---
+++ ProBlogWriter/frmMain.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+
+namespace ProBlogWriter
+{
+    public partial class frmMain : Form
+    {
+        private string version = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+
+        public frmMain()
+        {
+            InitializeComponent();
+            this.Text = this.Text + "  (" + version + ")";
+        }
+    }
+}
